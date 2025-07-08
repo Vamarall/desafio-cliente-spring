d515a7647a0e6a3a35e48171950e562201391b50
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-07-08 13:28:49 -0300

    Criada entidade Client

---
+++ src/main/java/com/example/demo/entities/Client.java
@@ -0,0 +1,88 @@
+package com.example.demo.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private LocalDate birthDate;
+    private Integer children;
+
+    public Client() {
+    }
+
+    public Client(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Client other = (Client) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Client [id=" + id + ", name=" + name + ", cpf=" + cpf + ", income=" + income + ", birthDate="
+                + birthDate + ", children=" + children + "]";
+    }
+
+   
+    
+}
