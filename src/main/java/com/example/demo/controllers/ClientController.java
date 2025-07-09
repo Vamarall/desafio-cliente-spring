98b0d14ed9c113d873872ddc4267e51e6ebcf862
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-07-09 17:37:08 -0300

    Criado metodo insert

--- src/main/java/com/example/demo/controllers/ClientController.java
+++ src/main/java/com/example/demo/controllers/ClientController.java
@@ -1,17 +1,22 @@
 package com.example.demo.controllers;
 
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.entities.Client;
 import com.example.demo.servicies.ClientService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -29,5 +34,18 @@ public class ClientController {
         return cliente.map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
+
+    @PostMapping()
+   public ResponseEntity<Client> insert(@RequestBody Client client){
+    Optional<Client> novoCliente = clientService.insert(client);
+      URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(novoCliente.get().getId())
+                .toUri();
+
+    return ResponseEntity.created(uri).body(novoCliente.get());
+
+   }
+    
     
 }
--- src/main/java/com/example/demo/servicies/ClientService.java
+++ src/main/java/com/example/demo/servicies/ClientService.java
@@ -8,15 +8,24 @@ import org.springframework.stereotype.Service;
 import com.example.demo.entities.Client;
 import com.example.demo.repositories.ClientRepository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class ClientService {
 
         @Autowired
     private ClientRepository clientRepository;
 
+    @Transactional
     public Optional<Client> findById(Long id){
         Optional<Client> client = clientRepository.findById(id);
         return client;
+    }
+
+    @Transactional
+    public Optional<Client> insert(Client client){
+        Client novoCliente = clientRepository.save(client);
+        return Optional.of(novoCliente);
     }
     
 }
